6fcbec0f551c7cfe65cc78807e673f88ba1cf0b4
David Gleba <dgleba@gmail.com>
2018-05-01 19:31:28 -0400

    theme switcher updated and working now 2018-05-01

--- index-link2themeswitcher.php
+++ index-link2themeswitcher.php
@@ -1,13 +1,5 @@
 <?php
 
-//don't show strict warnings.  xataface group.. Re: Getting messages: Strict Standards: Only variables should be assigned by reference errors
-//  ini_set('display_errors', '0');     # don't show any errors...
-//  error_reporting(E_ALL | E_STRICT);  # ...but do log them
-//report all errors for debugging...
-// error_reporting(E_ALL);
-// ini_set('display_errors', 'on');
-//
-
 function adminer_object()
 {
     // Required to run any plugin.
@@ -20,38 +12,19 @@ function adminer_object()
 
     // Specify enabled plugins here.
     $plugins = [
-    
         // new AdminerDatabaseHide(["mysql", "information_schema", "performance_schema"]),
         // new AdminerLoginServers([
             // filter_input(INPUT_SERVER, 'HTTP_HOST') => filter_input(INPUT_SERVER, 'SERVER_NAME')
         // ]),
-        // new AdminerLoginServers(),
-
         // new AdminerSimpleMenu(),
-        new AdminerCollations(),
-        new AdminerJsonPreview(),
+        // new AdminerCollations(),
+        // new AdminerJsonPreview(),
         
-        // these don't show on pematon adminer-custom..
-        
-        // adminer orig tables-filter
-        // new AdminerTablesFilter(),
-        // https://github.com/zhgabor/adminer-table-filter
-        // new AdminerQuickFilterTables,
-        
-        // https://github.com/arxeiss/Adminer-HideTables
-        new AdminerTablesHide(),
-        
-        
-        //  allow sqlite with login-sqlite plugin and selecting sqlite3 system type and put full path to sqlite database file in database field.
-        //  username and password on next line..
-        new AdminerLoginSqlite("sladmin", password_hash("sl", PASSWORD_DEFAULT)),
-
-        // pematon - AdminerTheme has to be the last one.
+        // AdminerTheme has to be the last one.
         // new AdminerTheme(),
-
+    ];
         
-   ];
-
+        
     /* It is possible to combine customization and plugins:
        // https://stackoverflow.com/questions/46186925/how-do-i-extend-adminer-to-support-sqlite-databases-with-login
         https://sourceforge.net/p/adminer/discussion/1095138/thread/7dcb8ebf/
@@ -72,9 +45,11 @@ function adminer_object()
     }
    
     return new AdminerCustomization($plugins);
-   
-   // return new AdminerPlugin($plugins);
+          
+
+    // return new AdminerPlugin($plugins);
 }
 
 // Include original Adminer or Adminer Editor.
 include "./adminer.php";
+
